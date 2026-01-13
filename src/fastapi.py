9642b6bc6457a82c7cb7ce494eb838414fa25f2d
anthonyafgx <anthonyafgx@gmail.com>
2026-01-13 16:15:21 -0400

    feat(employee): implement CRUD operations and API
    closes #14, closes #15, closes #16, closes #17, closes #18

--- src/fastapi.py
+++ src/fastapi.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from src.customer.api import router as customer_router
+from src.employee.api import router as employee_router
 
 app = FastAPI(title="CESA7000")
 
 app.include_router(customer_router)
+app.include_router(employee_router)
